c3c29dd480a6173df8fc59bb5bbe693651015d19
Skalik2 <106670239+Skalik2@users.noreply.github.com>
2023-10-13 20:36:38 +0200

    Dodaj pliki projektów.

---
+++ Symulacja Tkanki/Symulacja Tkanki.cpp
@@ -0,0 +1,24 @@
+﻿#include <SFML/Graphics.hpp>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(1200, 800), "");
+    sf::CircleShape shape(200.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+        
+        window.clear(sf::Color::Red);
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
+}
